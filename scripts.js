33664f8ed63d248ac23d02ec8416e5cd9fa190d1
GLancer14 <181259602+GLancer14@users.noreply.github.com>
2024-12-09 11:40:57 +0400

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const burgerButton = document.getElementsByClassName("header-hamburger")[0];
+const navMenu = document.getElementsByClassName("header-nav-list")[0];
+const authButtons = document.getElementsByClassName("header-auth-btns")[0];
+const signInButton = document.getElementsByClassName("header-sign-in-btn")[0];
+const signUpButton = document.getElementsByClassName("header-sign-up-btn")[0];
+const signInWindowClose = document.getElementsByClassName("sign-in-window-close")[0];
+const signInWindow = document.getElementsByClassName("sign-in-window-wrapper")[0];
+
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+mediaQuery.addEventListener("change", function (event) {
+  if (event.matches) {
+    authButtons.style.display = "none";
+    navMenu.style.display = "none";
+  } else {
+    authButtons.style.display = "flex";
+    navMenu.style.display = "block";
+  }
+})
+
+burgerButton.addEventListener("click", burgerClick);
+signInButton.addEventListener("click", headerSignInButtonCallback);
+signInWindowClose.addEventListener("click", closeSignInWindow);
+signInWindow.addEventListener("click", function (event) {
+  if (event.target !== event.currentTarget) return;
+  closeSignInWindow();
+});
+
+function burgerClick () {
+  if (navMenu.style.display === "none" || !navMenu.style.display) {
+    authButtons.style.display = "flex";
+    navMenu.style.display = "block";
+  } else {
+    authButtons.style.display = "none";
+    navMenu.style.display = "none";
+  }
+}
+
+function headerSignInButtonCallback () {
+  const signInWindowWrap = document.getElementsByClassName("sign-in-window-wrapper")[0];
+
+  if (signInWindowWrap.style.display === "none" || !signInWindowWrap.style.display) {
+    signInWindowWrap.style.display = "flex";
+  } else {
+    signInWindowWrap.style.display = "none";
+  }
+}
+
+function closeSignInWindow () {
+  signInWindow.style.display = "none";
+}
